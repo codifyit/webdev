6a1359c4b8767f6bbf21c966478903ec23b45ea3
rafamm <rmenmar264@g.educaand.es>
2025-11-13 14:29:20 +0100

    JS Script Calculator

---
+++ javascript/lib/math.js
@@ -0,0 +1,13 @@
+let result=0;
+function sum(n1,n2){
+    result = n1 + n2;
+}
+function substract(n1,n2){
+    result = n1 - n2;
+}
+function product(n1,n2){
+    result = n1 * n2;
+}
+function div(n1,n2){
+    result = n1 / n2;
+}
