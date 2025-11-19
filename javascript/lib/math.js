bef6f96ce9960ade510feeb2d5028780dd236adc
rafamm <rmenmar264@g.educaand.es>
2025-11-19 08:54:26 +0100

    Math lib for calculator

--- javascript/lib/math.js
+++ javascript/lib/math.js
@@ -1,6 +1,20 @@
-let result=0;
-function sum(n1,n2){
-    result = n1 + n2;
+let result=0; //result
+let op1=0; //operand 1
+let op2=0; //operand 2
+/*
+This function assign a value to op1
+*/
+function op1Assign(ope1){
+    op1 = ope1;
+}
+/*
+This function assign a value to op2
+*/
+function op2Assign(ope2){
+    op2 = ope2;
+}
+function sum(){
+    result = op1 + op2;
 }
 function substract(n1,n2){
     result = n1 - n2;
